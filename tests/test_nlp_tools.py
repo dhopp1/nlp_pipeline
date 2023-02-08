a5023a483f919a321665bbdf06ca06202ef038b1
Daniel Hopp <daniel.hopp1@gmail.com>
2023-02-08 09:40:53 +0100

    Adding file infrastructure

---
+++ tests/test_nlp_tools.py
@@ -0,0 +1,11 @@
+import unittest
+#from nlp_tools import *
+#from nlp_tools.module import *
+
+
+class TestDataSetup(unittest.TestCase):
+    def test_nlp_tools(self):
+        self.assertEqual(1, 1)
+
+if __name__ == "__main__":
+    unittest.main()
