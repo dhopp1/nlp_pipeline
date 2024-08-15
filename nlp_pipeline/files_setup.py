f156dad7a72868adea89d69043d49fa859ba490c
bakered <bakered91@gmail.com>
2024-08-15 17:27:51 +0200

    Update files_setup.py

--- nlp_pipeline/files_setup.py
+++ nlp_pipeline/files_setup.py
@@ -317,7 +317,7 @@ def convert_to_text(metadata, data_path, text_id, windows_tesseract_path = None,
     if raw_exists:
          raw_exists = raw_exists & (len(raw_path) > 0)
 
-    print("raw_exists"+raw_exists)
+    print("raw_exists"+str(raw_exists))
     if raw_exists:
         # first check if this file already converted
         if not(os.path.isfile(f"{data_path}txt_files/{text_id}.txt")):
